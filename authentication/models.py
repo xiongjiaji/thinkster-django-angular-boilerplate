ddac9ec6fdc6bc5fa39ed3184d6bd02a90de3c19
Kyle <jxiong@CH-C02MM1TZFH00.local>
2014-12-22 11:03:45 +0800

    User Registration
    User registration functionality is added.

---
+++ authentication/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+
+
+class AccountManager(BaseUserManager):
+    # Manager class for Account
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
+
+        account = self.model(
+            email=self.normalize_email(email), username=kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    """
+    If you define a __unicode__() method on your model and not a __str__() method,
+    Django will automatically provide you with a __str__() that calls __unicode__() and
+    then converts the result correctly to a UTF-8 encoded string object.
+    This is recommended development practice: define only __unicode__() and let Django take care of
+    the conversion to string objects when required.
+    """
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
